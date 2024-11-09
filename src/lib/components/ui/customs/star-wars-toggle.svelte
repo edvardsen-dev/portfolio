7db3b30749791360f50f9242deaf75a0005e294d
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2024-11-09 01:41:50 +0100

    feat: light saber theme toggle

---
+++ src/lib/components/ui/customs/star-wars-toggle.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+
+	let { checked = $bindable(false) }: { checked?: boolean } = $props();
+</script>
+
+<button class="flex w-[112px] items-center" onclick={() => (checked = !checked)}>
+	<img src="/img/luke-lightsaber.png" alt="Luke's lightsaber" class="z-10 w-8" />
+	{#if checked}
+		<div
+			in:slide={{ duration: 200, axis: 'x' }}
+			class="relative flex h-1 w-20 items-center rounded-full bg-green-400 pr-1"
+			style="box-shadow: 0 0 5px 5px #4ade80"
+		>
+			<div
+				class="absolute -left-1 h-0.5 w-20 rounded-full bg-white"
+				style="box-shadow: 0 0 5px 1px #fff"
+			></div>
+		</div>
+	{:else}
+		<div
+			class="invisible flex h-1 w-20 items-center rounded-full bg-green-400 px-1"
+			style="box-shadow: 0 0 5px 5px #4ade80"
+		>
+			<div class="h-0.5 w-full rounded-full bg-white" style="box-shadow: 0 0 5px 1px #fff"></div>
+		</div>
+	{/if}
+</button>
--- src/lib/components/ui/customs/theme-toggle.svelte
+++ src/lib/components/ui/customs/theme-toggle.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
 	import { Moon } from 'lucide-svelte';
 	import Sun from 'lucide-svelte/icons/sun';
-	import { mode, toggleMode } from 'mode-watcher';
+	import { mode, setMode, toggleMode } from 'mode-watcher';
+	import StarWarsToggle from './star-wars-toggle.svelte';
 
-	let checked = $derived($mode === 'dark');
-	let Icon = $derived($mode === 'dark' ? Moon : Sun);
+	let checked = $state($mode === 'dark');
+
+	$effect(() => {
+		checked ? setMode('dark') : setMode('light');
+	});
+
+	$effect(() => {
+		$mode === 'dark' ? (checked = true) : (checked = false);
+	});
 </script>
 
-<button
-	onclick={toggleMode}
-	class="relative h-6 w-12 rounded-full bg-neutral-300 dark:bg-neutral-700"
->
-	<div
-		class="absolute left-1 top-0 flex h-full items-center transition-transform {checked &&
-			'translate-x-5'}"
-	>
-		<Icon class="size-5" />
-	</div>
-</button>
+<StarWarsToggle bind:checked />
