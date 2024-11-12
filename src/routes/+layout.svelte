3e39c4ac49359bf52be4533cb20aa83c7aa9e4b7
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-11-12 16:55:44 +0100

    feat: meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,35 @@
 
 <svelte:head>
 	<title>Joakim Edvardsen</title>
+	<!--FavIcon  -->
 	<link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
+
+	<meta
+		name="description"
+		content="Joakim Edvardsen - Experienced Full-Stack Developer specializing in web application using modern JavaScript frameworks."
+	/>
+	<meta
+		name="keywords"
+		content="Front-End Developer, Full-Stack Developer, UX Designer, Web Developer, Responsive Design, Joakim Edvardsen"
+	/>
+
+	<meta property="og:title" content="Joakim Edvardsen" />
+	<meta
+		property="og:description"
+		content="Experienced developer specializing in web application using modern JavaScript frameworks."
+	/>
+	<meta property="og:url" content="https://www.edvardsen.dev" />
+	<meta property="og:type" content="website" />
+
+	<meta name="twitter:title" content="Joakim Edvardsenr" />
+	<meta
+		name="twitter:description"
+		content="Experienced developer specializing in web application using modern JavaScript frameworks."
+	/>
+
+	<meta name="author" content="Joakim Edvardsen" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
 
 <ModeWatcher />
